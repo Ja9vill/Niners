ffa5d4b2bc3285150cb97ce752ee2ba28cdb0801
Janine <jwavpr@gmail.com>
2026-06-01 04:19:42 -0400

    chore: remove IDE internals and debug scripts from repo, update .gitignore

--- check_col.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('src/components/DirectorTab.tsx', 'utf8').split('\n');
-console.log('Line 211:', lines[210]);
-for (let i=0; i<lines[210].length; i++) {
-  console.log(i+1, lines[210][i]);
-}
--- run_tsc.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  console.log('Running tsc...');
-  execSync('npx tsc --noEmit', { cwd: 'e:\\nine-dashboard', stdio: 'pipe' });
-  console.log('TSC passed');
-} catch (e) {
-  console.log('TSC Failed:\n', e.stdout.toString());
-}
