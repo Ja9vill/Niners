246f1fee348254db33d24d124f589d09bafd6b55
Janine WP <jwavpr@gmail.com>
2026-06-30 18:52:25 -0400

    chore: remove temporary debug scripts

--- check-syntax.ps1
+++
@@ -1,9 +0,0 @@
-$errors = $null
-$ast = [System.Management.Automation.Language.Parser]::ParseFile('deploy-all.ps1', [ref]$null, [ref]$errors)
-if ($errors.Count -gt 0) {
-    foreach ($e in $errors) { Write-Host "ERROR at line $($e.Extent.StartLineNumber): $($e.Message)" }
-    exit 1
-} else {
-    Write-Host "Syntax OK - no parse errors"
-    exit 0
-}
